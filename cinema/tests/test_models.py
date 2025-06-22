013c93854c751dd024920a7caecc24379507465e
Brandon Goding <brandon.h.goding@gmail.com>
2025-06-22 15:41:00 -0400

    tests(booking): booking tests

--- cinema/tests/test_models.py
+++ cinema/tests/test_models.py
@@ -25,3 +25,41 @@ class FilmModelTest(TestCase):
         self.film.slug = None
         self.film.save()
         mock_slugify.assert_called_once_with("A Good Film")
+
+
+class BookingModelTest(TestCase):
+    def setUp(self) -> None:
+        self.film = Film.objects.create(title="A Good Film", imdb_id="1234", youtube_id="5678")
+        self.booking = self.film.bookings.create(
+            booking_start_date="2023-01-01",
+            booking_end_date="2023-01-10",
+            confirmed=True,
+        )
+
+    def test_booking_str(self) -> None:
+        expected_str = (
+            f"Booking for {self.film.title} from {self.booking.booking_start_date} "
+            f"to {self.booking.booking_end_date}"
+        )
+        self.assertEqual(str(self.booking), expected_str)
+
+    def test_is_active(self) -> None:
+        with mock.patch("django.utils.timezone.now") as mock_now:
+            mock_now.return_value = mock.Mock(date=lambda: "2023-01-05")
+            self.assertTrue(self.booking.is_active)
+
+    def test_is_not_active(self) -> None:
+        with mock.patch("django.utils.timezone.now") as mock_now:
+            # Case 1: Before booking start date
+            mock_now.return_value = mock.Mock(date=lambda: "2022-12-31")
+            self.assertFalse(self.booking.is_active)
+            # Case 2: After booking end date
+            mock_now.return_value = mock.Mock(date=lambda: "2023-01-11")
+            self.assertFalse(self.booking.is_active)
+
+    def test_is_confirmed(self) -> None:
+        self.assertTrue(self.booking.is_confirmed)
+
+    def test_is_not_confirmed(self) -> None:
+        self.booking.confirmed = False
+        self.assertFalse(self.booking.is_confirmed)
