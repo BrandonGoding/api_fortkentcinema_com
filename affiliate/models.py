81593253d935e1d50ae49e210000f86b9aded93d
Brandon H. Goding <brandon.h.goding@gmail.com>
2025-09-12 12:20:35 -0400

    feat(affiliate): new affialiate app

---
+++ affiliate/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    link = models.URLField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to="product/")
+
