7decaf2a5633a78daffe72d88a105b6c685511ac
Brandon H. Goding <brandon.h.goding@gmail.com>
2025-09-12 14:02:50 -0400

    feat(affiliate): new affialiate app

--- affiliate/models.py
+++ affiliate/models.py
@@ -5,5 +5,5 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     link = models.URLField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to="product/")
+    image = models.ImageField(upload_to="affiliate/product/img/")
 
--- fortkentcinema/urls.py
+++ fortkentcinema/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -30,4 +30,4 @@ if settings.DEBUG:
     # Include django_browser_reload URLs only in DEBUG mode
     urlpatterns += [
         path("__reload__/", include("django_browser_reload.urls")),
-    ]
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
