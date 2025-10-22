284f8b11a514bdf3a998f341eaefc1adf25a7e5b
Brandon H. Goding <brandon.h.goding@gmail.com>
2025-10-22 10:37:02 -0400

    chore(admin): add film genre to admin screen

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from cinema.models import Booking, Event, Film, ScreeningTime
+from cinema.models import Booking, Event, Film, ScreeningTime, FilmGenre
 
 
 class ScreeningTimeInline(admin.TabularInline):
@@ -14,4 +14,5 @@ class BookingAdmin(admin.ModelAdmin):
 
 admin.site.register(Event)
 admin.site.register(Film)
+admin.site.register(FilmGenre)
 admin.site.register(Booking, BookingAdmin)
