a4940454acf3ecfa60ff177a831125eb0a47973b
Brandon H. Goding <brandon.h.goding@gmail.com>
2025-11-14 08:37:43 -0500

    feat(items): sync items

--- square_integration/models.py
+++ square_integration/models.py
@@ -99,7 +99,7 @@ class CatalogCategory(models.Model):
 
 class TaxRate(models.Model):
     name = models.CharField(max_length=100)
-    percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    percentage = models.DecimalField(max_digits=5, decimal_places=3)
     square_id = models.CharField(max_length=255, blank=True, null=True)
     active = models.BooleanField(default=True)
 
