c10dd263cec7db9b9daf49f1ad3d6a67ca9fec10
Brandon H. Goding <brandon.h.goding@gmail.com>
2025-11-15 21:52:28 -0500

    refactor(square): simplify square

--- square_integration/models.py
+++ square_integration/models.py
@@ -74,24 +74,52 @@ class CatalogObject(BaseModel):
     version: int | None = None
 
 
+class SquareItem(models.Model):
+    square_id = models.CharField(max_length=255, blank=True, null=True)
+    version = models.BigIntegerField(blank=True, null=True)
+
+    def get_item_type(self) -> CatalogObjectType:
+        raise NotImplementedError("Subclasses must implement get_item_type method")
+
+
 # Django models unchanged
-class CatalogCategory(models.Model):
+class CatalogCategory(SquareItem):
     name = models.CharField(max_length=255)
     parent = models.ForeignKey(
         "self", on_delete=models.CASCADE, blank=True, null=True, related_name="subcategories"
     )
-    square_id = models.CharField(max_length=255, blank=True, null=True)
     active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
 
+    def get_item_type(self) -> CatalogObjectType:
+        return CatalogObjectType.CATEGORY
 
-class TaxRate(models.Model):
+
+class TaxRate(SquareItem):
     name = models.CharField(max_length=100)
     percentage = models.DecimalField(max_digits=5, decimal_places=3)
-    square_id = models.CharField(max_length=255, blank=True, null=True)
     active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.name} ({self.percentage}%)"
+
+    def get_item_type(self) -> CatalogObjectType:
+        return CatalogObjectType.TAX
+
+
+class Item(SquareItem):
+    name = models.CharField(max_length=255, default="Regular Item")
+    description = models.TextField(blank=True, null=True, default="This is the default variation of this item")
+    abbreviation = models.CharField(max_length=10, blank=True, null=True)
+    categories = models.ManyToManyField(CatalogCategory, related_name="items", blank=True)
+    is_taxable = models.BooleanField(default=False)
+    tax_rates = models.ManyToManyField(TaxRate, related_name="items", blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_item_type(self) -> CatalogObjectType:
+        return CatalogObjectType.ITEM
